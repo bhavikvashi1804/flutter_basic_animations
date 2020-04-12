54544d5086c319036f0cf3b6de42f9065ffe7237
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-04-12 13:27:28 +0530

    Brick Animation: 03 Provided Animations for each interval

--- lib/brick_anim.dart
+++ lib/brick_anim.dart
@@ -5,7 +5,93 @@ class BrickPage extends StatefulWidget {
   _BrickPageState createState() => _BrickPageState();
 }
 
-class _BrickPageState extends State<BrickPage> {
+class _BrickPageState extends State<BrickPage> with TickerProviderStateMixin{
+
+  AnimationController _animationController;
+  Tween _tween;
+
+  @override
+  void initState() {
+   
+    _animationController=AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 20)
+    );
+
+    _tween=Tween(begin: 0.0,end: 1.0);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() { 
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  //for B1
+  Animation get animOne=>_tween.animate(
+    CurvedAnimation(
+      parent: _animationController, 
+      curve: Interval(0.0, 0.125,curve: Curves.linear),
+    )
+  );
+  //for B1
+  Animation get animTwo=>_tween.animate(
+    CurvedAnimation(
+      parent: _animationController, 
+      curve: Interval(0.125, 0.250,curve: Curves.linear),
+    )
+  );
+
+  //for B2
+  Animation get animThree=>_tween.animate(
+    CurvedAnimation(
+      parent: _animationController, 
+      curve: Interval(0.250, 0.375,curve: Curves.linear),
+    )
+  );
+  //for B3
+   Animation get animFour=>_tween.animate(
+    CurvedAnimation(
+      parent: _animationController, 
+      curve: Interval(0.375, 0.5,curve: Curves.linear),
+    )
+  );
+
+  //for B4
+  Animation get animFive=>_tween.animate(
+    CurvedAnimation(
+      parent: _animationController, 
+      curve: Interval(0.5, 0.625,curve: Curves.linear),
+    )
+  );
+  //for B4
+  Animation get animSix=>_tween.animate(
+    CurvedAnimation(
+      parent: _animationController, 
+      curve: Interval(0.625, 0.750,curve: Curves.linear),
+    )
+  );
+
+  //for B3
+  Animation get animSeven=>_tween.animate(
+    CurvedAnimation(
+      parent: _animationController, 
+      curve: Interval(0.750, 0.875,curve: Curves.linear),
+    )
+  );
+
+  //for B2
+  Animation get animEight=>_tween.animate(
+    CurvedAnimation(
+      parent: _animationController, 
+      curve: Interval(0.875, 1.0,curve: Curves.linear),
+    )
+  );
+
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
