dba5cdb66420a284fc05b62849c5e04f2b25ad9f
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-04-12 14:22:03 +0530

    Animate

--- lib/flipper.dart
+++ lib/flipper.dart
@@ -1,11 +1,52 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class FlipperPage extends StatefulWidget {
   @override
   _FlipperPageState createState() => _FlipperPageState();
 }
 
-class _FlipperPageState extends State<FlipperPage> {
+class _FlipperPageState extends State<FlipperPage> with SingleTickerProviderStateMixin {
+
+  bool _reversed=false;
+  Animation<double> _animation;
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    
+    _animationController=AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 2000)
+    );
+
+     _animation = TweenSequence([
+      TweenSequenceItem(tween: Tween(begin: 0.0, end: -math.pi / 2), weight: 0.5),
+      TweenSequenceItem(tween: Tween(begin: math.pi / 2, end: 0.0), weight: 0.5)
+    ]).animate(_animationController);
+  
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  _doAnim() {
+    if (!mounted) return;
+    if (_reversed) {
+      _animationController.reverse();
+      _reversed = false;
+    } else {
+      _animationController.forward();
+      _reversed = true;
+    }
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,9 +54,70 @@ class _FlipperPageState extends State<FlipperPage> {
         title: Text('Flipper'),
       ),
       body: Center(
-        child:Text('Flipper Demo'),
+        child: AnimatedBuilder(
+          animation: _animation,
+          builder: (context, child) => Transform(
+                alignment: Alignment.center,
+                transform: Matrix4.identity()
+                  ..setEntry(3, 2, 0.001)
+                  ..rotateY(_animation.value),
+                child: GestureDetector(
+                  onTap: _doAnim,
+                  child: IndexedStack(
+                    children: <Widget>[CardOne(), CardTwo()],
+                    alignment: Alignment.center,
+                    index: _animationController.value < 0.5 ? 0 : 1,
+                  ),
+                ),
+              ),
+        ),
       ),
-      
+    );
+  }
+}
+
+class CardOne extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.red,
+      child: Container(
+        width: 300,
+        height: 300,
+        child: Center(
+          child: Text(
+            "Tap to see the code",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 25.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CardTwo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.black,
+      child: Container(
+        width: 300,
+        height: 300,
+        child: Center(
+          child: Text(
+            "356789",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 35.0,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
